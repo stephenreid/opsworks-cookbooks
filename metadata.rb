de59fd33edd00959b55778237ea1931f65a5d90e
Adam Griffis <abgriff@gmail.com>
2014-02-23 20:03:31 -0600

    Still figuring things out.

--- metadata.rb
+++ metadata.rb
@@ -3,11 +3,5 @@ maintainer_email "abgriff@gmail.com"
 license          "MIT"
 description      "Opsworks cookbooks."
 
-name   'opsworks_sidekiq'
-recipe 'opsworks_sidekiq::setup',     'Set up sidekiq worker.'
-recipe 'opsworks_sidekiq::configure', 'Configure sidekiq worker.'
-recipe 'opsworks_sidekiq::deploy',    'Deploy sidekiq worker.'
-recipe 'opsworks_sidekiq::undeploy',  'Undeploy sidekiq worker.'
-recipe 'opsworks_sidekiq::stop',      'Stop sidekiq worker.'
-
-name 'whenever'
+depends "opsworks_sidekiq"
+depends "whenever"
